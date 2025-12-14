f7d637547103e4ee360b16c4c2f99fb8a12da595
CallmeSen <hthanhtuan102@gmail.com>
2025-12-14 16:35:34 +0700

    fix jest config in package.json

--- backend/src/modules/auth/auth.controller.spec.ts
+++ backend/src/modules/auth/auth.controller.spec.ts
@@ -1,14 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserService } from 'src/modules/users/users.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
+  const authServiceMock = {
+    Register: jest.fn(),
+    login: jest.fn(),
+    AdminLogin: jest.fn(),
+    LogOut: jest.fn(),
+    refreshToken: jest.fn(),
+    resetPassword: jest.fn(),
+    generateAccesstoken: jest.fn(),
+    generateToken: jest.fn(),
+    ResetPassword: jest.fn(),
+    checkPostcode: jest.fn(),
+  };
+
+  const userServiceMock = {
+    checkEmailExist: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: UserService, useValue: userServiceMock },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
